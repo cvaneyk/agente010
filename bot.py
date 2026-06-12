1d63f1286ae6615ed4b91aef0145ca3e81e787cf
cvaneyk <144983285+cvaneyk@users.noreply.github.com>
2026-06-12 09:24:21 +0200

    Remove auto_hang_up option from TwilioFrameSerializer

--- bot.py
+++ bot.py
@@ -54,7 +54,6 @@ async def run_bot(websocket, stream_sid: str):
             vad_audio_passthrough=True,
             serializer=TwilioFrameSerializer(
                 stream_sid=stream_sid,
-                auto_hang_up=False,
             ),
         ),
     )
